67b0e53b31b677f9f57431eaba052e2b18fec953
rohandhamshala <133068761+rohandhamshala@users.noreply.github.com>
2023-05-29 17:52:26 +0000

    add Trips,Events component along with Service file

---
+++ src/services/TripServices.js
@@ -0,0 +1,32 @@
+import apiClient from "./services";
+
+export default {
+  getTrips() {
+    return apiClient.get("trips");
+  },
+  getTrip(id) {
+    return apiClient.get("trips/" + id);
+  },
+  addTrip(trip) {
+    return apiClient.post("trips", trip);
+  },
+  updateTrip(trip) {
+    return apiClient.put("trips/" + trip.id, trip);
+  },
+  deleteTrip(tripId) {
+    return apiClient.delete("trips/" + tripId);
+  },
+  // Get all latest itineraries
+  getLatestTrips() {
+    return apiClient.delete("trips/special/latest");
+  },
+  getHighRatedTrips() {
+    return apiClient.delete("trips/special/high-rated");
+  },
+  getPlanOfTrip(tripId) {
+    return apiClient.delete("trips/special/"+tripId+"/plan");
+  },
+  getDayDetailsOfTrip(tripId,dayId) {
+    return apiClient.delete("trips/"+tripId+"/days/"+dayId);
+  }
+};
